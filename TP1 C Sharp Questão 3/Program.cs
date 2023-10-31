125becf19654403276b6a77494cfbf6df9909861
Marcos Carvalhido Rodrigues <marcoscrodrigues8@gmail.com>
2023-10-31 19:15:30 -0300

    Continuação da questão 2 e 3.

---
+++ TP1 C Sharp Questão 3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TP1_C_Sharp_Questão_3
+    /*
+     * Uma progressão aritmética (PA) é uma sequência numérica em que cada termo, a partir do segundo, 
+     * é igual a soma do termo anterior com uma constante. A constante é calculada como sendo a diferença entre o segundo e o primeiro número. 
+     * Faça um programa que receba dois números inteiros e, a partir dessa informação, gere uma sequência em progressão aritmética.
+     * */
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
